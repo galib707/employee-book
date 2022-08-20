41f2c47a18a5f1cf97da67dc3007060df2bfaf3d
galib <ultansms@gmail.com>
2022-08-21 02:36:20 +0600

    style added

--- src/components/UserCard.jsx
+++ src/components/UserCard.jsx
@@ -1,24 +1,17 @@
-
 function UserCard(props) {
-    let { title, first, last } = props.user.name
-    let { email } = props.user
-    let { thumbnail } = props.user.picture
+  let { title, first, last } = props.user.name;
+  let { email } = props.user;
+  let { thumbnail } = props.user.picture;
 
-    return (
-        <div className="card">
-            <img src={thumbnail} className="photo" alt="" />
-            <div className="name-emai">
-                <p> {`${title} ${first} ${last}`} </p>
-                <p>{email}</p>
-            </div>
-
-        </div>
-
-
-    )
-
-
+  return (
+    <div className="card">
+      <img src={thumbnail} className="photo" alt="" />
+      <div className="name-emai">
+        <p> {`${title} ${first} ${last}`} </p>
+        <p>{email}</p>
+      </div>
+    </div>
+  );
 }
-
 
 export default UserCard;
